92d82c2fa302d8b8682a6b78a73568e906fc96b9
LeoDreamer <2200010825@stu.pku.edu.cn>
2024-12-13 20:17:17 +0800

    add: train()

--- code/params.py
+++ code/params.py
@@ -1,3 +1,10 @@
-ITERATIONS = 1000
+import torch
+
+DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+MODEL_PATH = "../model/transformer.pth"
+INPUT_SIZE = 7  # [<eos>, num, +, num, =, num, <eos>]
+
+# Hyperparameters
+EPOCH_NUM = 1000
 BATCHSIZE = 100
 TEST_ALPHA = 0.2
