b55e36d6a80a9736955fc82112b3ddb765d44987
LeoDreamer <2200010825@stu.pku.edu.cn>
2024-12-20 14:45:37 +0800

    add: config json

--- code/src/modular_add/params.py
+++ code/src/modular_add/params.py
@@ -1,17 +1,53 @@
 import torch
+import json
+from dataclasses import dataclass
 
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-MODEL_PATH = "../model/transformer.pth"
 
-# Hyperparameters
-EPOCH_NUM = 50000
-LEARNING_RATE = 0.001
-BATCH_SIZE = 200
-TEST_ALPHA = 0.5
-D_MODEL = 16
-N_HEAD = 1
-DIM_FEEDFORWARD = 32
-N_LAYERS = 4
-MAX_SEQ_LENGTH = 8
-DROPOUT = 0.
-MODULUS = 97
+@dataclass
+class Param:
+    MODEL_PATH = "../model/transformer.pth"
+    MODULUS = 47
+
+    # Default Hyperparameters
+    SEED = 0
+    EPOCH_NUM = 50000
+    LR = 0.001
+    BATCH_SIZE = 256
+    TEST_ALPHA = 0.4
+    D_MODEL = 16
+    N_HEAD = 1
+    DIM_FEEDFORWARD = 32
+    N_LAYERS = 4
+    MAX_SEQ_LENGTH = 8
+    DROPOUT = 0.
+    STEP_LR_STEP_SIZE = 100
+    STEP_LR_GAMMA = 0.98
+    LOG_INTERVAL = 10
+    SAVE_INTERVAL = 100
+
+
+def load_params(path=None):
+
+    if path is None:
+        return 
+    with open(path) as f:
+        params = json.load(f)
+    print("Loaded params from:", path)
+    
+    Param.MODEL_PATH = params.get("model_path", Param.MODEL_PATH)
+    Param.MODULUS = params.get("modulus", Param.MODULUS)
+    Param.EPOCH_NUM = params.get("epoch_num", Param.EPOCH_NUM)
+    Param.LR = params.get("lr", Param.LR)
+    Param.BATCH_SIZE = params.get("batch_size", Param.BATCH_SIZE)
+    Param.TEST_ALPHA = params.get("test_alpha", Param.TEST_ALPHA)
+    Param.D_MODEL = params.get("d_model", Param.D_MODEL)
+    Param.N_HEAD = params.get("n_head", Param.N_HEAD)
+    Param.DIM_FEEDFORWARD = params.get("dim_feedforward",Param.DIM_FEEDFORWARD)
+    Param.N_LAYERS = params.get("n_layers", Param.N_LAYERS)
+    Param.MAX_SEQ_LENGTH = params.get("max_seq_length", Param.MAX_SEQ_LENGTH)
+    Param.DROPOUT = params.get("dropout", Param.DROPOUT)
+    Param.STEP_LR_STEP_SIZE = params.get("step_lr_step_size", Param.STEP_LR_STEP_SIZE)
+    Param.STEP_LR_GAMMA = params.get("step_lr_gamma", Param.STEP_LR_GAMMA)
+    Param.LOG_INTERVAL = params.get("log_interval", Param.LOG_INTERVAL)
+    Param.SAVE_INTERVAL = params.get("save_interval", Param.SAVE_INTERVAL)
--- code/src/train.py
+++ code/src/train.py
@@ -4,23 +4,40 @@ from torch.utils.data import DataLoader
 import matplotlib.pyplot as plt
 import numpy as np
 
+import random
+import argparse
+
 from modular_add.data import AlgorithmDataSet
 from modular_add.model import TransformerModel
 from modular_add.params import *
 
 
-def load_model(n_token: int) -> TransformerModel:
-    model = TransformerModel(n_token, d_model=D_MODEL, n_head=N_HEAD, n_layers=N_LAYERS,
-                             max_seq_length=MAX_SEQ_LENGTH, dim_feedforward=DIM_FEEDFORWARD).to(DEVICE)
-    model.load_state_dict(torch.load(MODEL_PATH))
+def init_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--param", type=str, help="The path to the params")
+    namespace = parser.parse_args()
+    path = namespace.param
+    load_params(path)
+
+
+def seed():
+    torch.manual_seed(Param.SEED)
+    np.random.seed(Param.SEED)
+    random.seed(Param.SEED)
+
+
+def load_model(n_token: int):
+    model = TransformerModel(n_token, d_model=Param.D_MODEL, n_head=Param.N_HEAD, n_layers=Param.N_LAYERS,
+                             max_seq_length=Param.MAX_SEQ_LENGTH, dim_feedforward=Param.DIM_FEEDFORWARD).to(DEVICE)
+    model.load_state_dict(torch.load(Param.MODEL_PATH))
     return model
 
 
 def save_model(model: TransformerModel):
-    torch.save(model.state_dict(), MODEL_PATH)
+    torch.save(model.state_dict(), Param.MODEL_PATH)
 
 
-def accuracy(data_loader: DataLoader, model: nn.Module) -> float:
+def accuracy(data_loader: DataLoader, model: nn.Module):
     with torch.no_grad():
         correct = 0
         total = 0
@@ -34,34 +51,33 @@ def accuracy(data_loader: DataLoader, model: nn.Module) -> float:
 
 
 def train():
-    torch.manual_seed(0)
-    np.random.seed(0)
+    seed()
 
-    dataset = AlgorithmDataSet(MODULUS)
-    train_data, test_data = train_test_split(dataset, test_size=TEST_ALPHA)
-    train_dataloader = DataLoader(train_data, batch_size=BATCH_SIZE, shuffle=True)
+    # Initialize data
+    dataset = AlgorithmDataSet(Param.MODULUS)
+    print("Dataset initialized. Data size: ", len(dataset))
+    train_data, test_data = train_test_split(dataset, test_size=Param.TEST_ALPHA)
+    train_dataloader = DataLoader(train_data, batch_size=Param.BATCH_SIZE, shuffle=True)
     test_dataloader = DataLoader(test_data, batch_size=len(test_data), shuffle=True)
 
     n_token = len(dataset.tokenizer)
-
-    model = TransformerModel(n_token, d_model=D_MODEL, n_head=N_HEAD, n_layers=N_LAYERS,
-                             max_seq_length=MAX_SEQ_LENGTH, dim_feedforward=DIM_FEEDFORWARD).to(DEVICE)
-    optimizer = optim.Adam(model.parameters(), lr=LEARNING_RATE)
+    model = TransformerModel(n_token, d_model=Param.D_MODEL, n_head=Param.N_HEAD, n_layers=Param.N_LAYERS,
+                             max_seq_length=Param.MAX_SEQ_LENGTH, dim_feedforward=Param.DIM_FEEDFORWARD).to(DEVICE)
+    optimizer = optim.Adam(model.parameters(), lr=Param.LR)
     criterion = nn.CrossEntropyLoss()
-
-    scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=100, gamma=0.98)
+    scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=Param.STEP_LR_STEP_SIZE, gamma=Param.STEP_LR_GAMMA)
 
     losses = []
     train_accuracy_list = []
     test_accuracy_list = []
-    log_interval = 10
 
-    for epoch in range(EPOCH_NUM):
+    for epoch in range(Param.EPOCH_NUM):
         epoch_loss = 0
         for i, (lhs, rhs) in enumerate(train_dataloader):
             labels = rhs.argmax(dim=2).reshape(-1)
             optimizer.zero_grad()
-            output = model.forward(lhs)  # Type: ignore
+            output = model.forward(lhs)  # Type: ignore    
+
             loss = criterion.forward(output[:, -1, :], labels)
             epoch_loss += loss.item()
             loss.backward()
@@ -70,29 +86,39 @@ def train():
 
         losses.append(epoch_loss)
 
-        if (epoch + 1) % log_interval == 0:
+        if (epoch + 1) % Param.LOG_INTERVAL == 0:
             train_accuracy = accuracy(train_dataloader, model)
             test_accuracy = accuracy(test_dataloader, model)
             train_accuracy_list.append(train_accuracy)
             test_accuracy_list.append(test_accuracy)
             print(
-                f"Epoch: {epoch + 1}, Loss: {epoch_loss:.6e}, Train accuracy: {train_accuracy * 100:.4f} %, Test accuracy: {test_accuracy * 100:.4f} %")
+                f"Epoch: {epoch + 1}",
+                f"Loss: {epoch_loss:.6e}", 
+                f"Train accuracy: {train_accuracy * 100:.4f}%",
+                f"Test accuracy: {test_accuracy * 100:.4f}%"
+            )
+            
+        if (epoch + 1) % Param.SAVE_INTERVAL == 0:
+            save_model(model)
+            print("Saved model at epoch", epoch + 1)
 
     save_model(model)
+    print("Training finished.")
     plt.plot(losses)
     plt.xlabel("Epoch")
     plt.ylabel("Loss")
     plt.xscale("log")
-    plt.show()
-    x = range(1, EPOCH_NUM + 1, log_interval)
+    plt.savefig("loss.png")
+    x = range(1, Param.EPOCH_NUM + 1, Param.LOG_INTERVAL)
     plt.plot(x, train_accuracy_list, label="train")
     plt.plot(x, test_accuracy_list, label="test")
     plt.xlabel("Epoch")
     plt.ylabel("Accuracy")
     plt.legend()
     plt.xscale("log")
-    plt.show()
+    plt.savefig("acc.png")
 
 
 if __name__ == "__main__":
+    init_args()
     train()
