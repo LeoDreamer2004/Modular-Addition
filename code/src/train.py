9b0e808a3f64a891d80beadd84c9eed94cf8bc7c
crl <crl18039102576@126.com>
2024-12-25 21:56:21 +0800

    [add] full batch grokking

--- code/src/train.py
+++ code/src/train.py
@@ -60,8 +60,8 @@ def save_fig(trained_epoch: int, train_losses: List, train_acc: List, test_losse
     save_path = os.path.join(Param.FIGURE_SAVE_PATH, Param.MODEL.lower())
     if not os.path.exists(save_path):
         os.makedirs(save_path)
-    if Param.DROPOUT > 0:
-        suffix = f"{Param.OPTIMIZER.lower()}-{Param.TEST_ALPHA}-{Param.NUM_ADDER}-dropout"
+    if Param.FIG_SUFFIX is not None:
+        suffix = f"{Param.OPTIMIZER.lower()}-{Param.TEST_ALPHA}-{Param.NUM_ADDER}-{Param.FIG_SUFFIX}"
     else:
         suffix = f"{Param.OPTIMIZER.lower()}-{Param.TEST_ALPHA}-{Param.NUM_ADDER}"
     x = range(Param.LOG_INTERVAL, trained_epoch + 1 + Param.LOG_INTERVAL, Param.LOG_INTERVAL)
